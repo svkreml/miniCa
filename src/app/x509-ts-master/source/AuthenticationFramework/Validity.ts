2c88ff7ee72049523eccb12aa4a68b8ddf609524
svkreml <svkreml@gmail.com>
2019-09-05 15:31:48 +0300

    fix date encoding utc

--- src/app/x509-ts-master/source/AuthenticationFramework/Validity.ts
+++ src/app/x509-ts-master/source/AuthenticationFramework/Validity.ts
@@ -69,12 +69,36 @@ class Validity {
     }
 
     public toElement(): DERElement {
+
+
+
+
+        /*
+        CAs conforming to this profile MUST always encode certificate
+        validity dates through the year 2049 as UTCTime; certificate validity
+        dates in 2050 or later MUST be encoded as GeneralizedTime.
+        Conforming applications MUST be able to process validity dates that
+        are encoded in either UTCTime or GeneralizedTime.
+        * */
+
         const notBeforeElement: DERElement = new DERElement();
-        notBeforeElement.tagNumber = ASN1UniversalType.generalizedTime;
-        notBeforeElement.generalizedTime = this.notBefore;
+        if (this.notBefore.getFullYear() < 2049) {
+            notBeforeElement.utcTime = this.notBefore;
+            notBeforeElement.tagNumber = ASN1UniversalType.utcTime;
+        } else {
+            notBeforeElement.generalizedTime = this.notBefore;
+            notBeforeElement.tagNumber = ASN1UniversalType.generalizedTime;
+        }
+
         const notAfterElement: DERElement = new DERElement();
-        notAfterElement.tagNumber = ASN1UniversalType.generalizedTime;
-        notAfterElement.generalizedTime = this.notBefore;
+        if (this.notAfter.getFullYear() < 2049) {
+            notAfterElement.utcTime = this.notAfter;
+            notAfterElement.tagNumber = ASN1UniversalType.utcTime;
+        } else {
+            notAfterElement.generalizedTime = this.notAfter;
+            notAfterElement.tagNumber = ASN1UniversalType.generalizedTime;
+        }
+
         const validityElement: DERElement = new DERElement();
         validityElement.tagClass = ASN1TagClass.universal;
         validityElement.construction = ASN1Construction.constructed;
