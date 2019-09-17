03ca5c7ddef76cc4916cb173303ba6ff220e4501
svkreml <svkreml@gmail.com>
2019-09-17 19:48:46 +0300

    add more algs for validator

--- src/app/validate-certificate/validate-certificate.component.ts
+++ src/app/validate-certificate/validate-certificate.component.ts
@@ -1,8 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {Certificate} from '../x509-ts-master/source/AuthenticationFramework';
+import {Certificate, SubjectPublicKeyInfo} from '../x509-ts-master/source/AuthenticationFramework';
 import {Base64} from '../gost-crypto/gost-coding/gost-coding';
 import TBSCertificate from '../x509-ts-master/source/AuthenticationFramework/TBSCertificate';
 import {CryptoModule} from '../crypto-module';
+import AlgorithmIdentifier from '../x509-ts-master/source/AuthenticationFramework/AlgorithmIdentifier';
+
 
 @Component({
     selector: 'app-validate-certificate',
@@ -12,26 +14,7 @@ import {CryptoModule} from '../crypto-module';
 export class ValidateCertificateComponent implements OnInit {
 
     output: string;
-    inputCert: string =
-        'MIIDXzCCAkegAwIBAgILBAAAAAABIVhTCKIwDQYJKoZIhvcNAQELBQAwTDEgMB4G\n' +
-        'A1UECxMXR2xvYmFsU2lnbiBSb290IENBIC0gUjMxEzARBgNVBAoTCkdsb2JhbFNp\n' +
-        'Z24xEzARBgNVBAMTCkdsb2JhbFNpZ24wHhcNMDkwMzE4MTAwMDAwWhcNMjkwMzE4\n' +
-        'MTAwMDAwWjBMMSAwHgYDVQQLExdHbG9iYWxTaWduIFJvb3QgQ0EgLSBSMzETMBEG\n' +
-        'A1UEChMKR2xvYmFsU2lnbjETMBEGA1UEAxMKR2xvYmFsU2lnbjCCASIwDQYJKoZI\n' +
-        'hvcNAQEBBQADggEPADCCAQoCggEBAMwldpB5BngiFvXAg7aEyiie/QV2EcWtiHL8\n' +
-        'RgJDx7KKnQRfJMsuS+FggkbhUqsMgUdwbN1k0ev1LKMPgj0MK66X17YUhhB5uzsT\n' +
-        'gHeMCOFJ0mpiLx9e+pZo34knlTifBtc+ycsmWQ1z3rDI6SYOgxXG71uL0gRgykmm\n' +
-        'KPZpO/bLyCiR5Z2KYVc3rHQU3HTgOu5yLy6c+9C7v/U9AOEGM+iCK65TpjoWc4zd\n' +
-        'QQ4gOsC0p6Hpsk+QLjJg6VfLuQSSaGjlOCZgdbKfd/+RFO+uIEn8rUAVSNECMWEZ\n' +
-        'XriX7613t2Saer9fwRPvm2L7DWzgVGkWqQPabumDk3F2xmmFghcCAwEAAaNCMEAw\n' +
-        'DgYDVR0PAQH/BAQDAgEGMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFI/wS3+o\n' +
-        'LkUkrk1Q+mOai97i3Ru8MA0GCSqGSIb3DQEBCwUAA4IBAQBLQNvAUKr+yAzv95ZU\n' +
-        'RUm7lgAJQayzE4aGKAczymvmdLm6AC2upArT9fHxD4q/c2dKg8dEe3jgr25sbwMp\n' +
-        'jjM5RcOO5LlXbKr8EpbsU8Yt5CRsuZRj+9xTaGdWPoO4zzUhw8lo/s7awlOqzJCK\n' +
-        '6fBdRoyV3XpYKBovHd7NADdBj+1EbddTKJd+82cEHhXXipa0095MJ6RMG3NzdvQX\n' +
-        'mcIfeg7jLQitChws/zyrVQ4PkX4268NXSb7hLi18YIvDQVETI53O9zJrlAGomecs\n' +
-        'Mx86OyXShkDOOyyGeMlhLxS67ttVb9+E7gUJTb0o2HLO02JQZR7rkpeDMdmztcpH\n' +
-        'WD9f';
+    inputCert: string = '';
 
     constructor() {
     }
@@ -59,14 +42,52 @@ export class ValidateCertificateComponent implements OnInit {
         return outputBytes;
     }
 
-    async onSubmit() {
-        let inputCert = this.inputCert;
-
-        let result: boolean = await ValidateCertificateComponent.validateCert(inputCert);
-        this.output = result ? 'valid' : 'invalid';
-    }
-
-    ngOnInit(): void {
+    static algorithmToCryptoSubtleMapper(algorithmIdentifier: AlgorithmIdentifier): string | RsaHashedImportParams | EcKeyImportParams | HmacImportParams | DhImportKeyParams | AesKeyAlgorithm {
+        switch (algorithmIdentifier.algorithm.toString()) {
+            case '1.2.840.113549.1.1.11':
+                return {
+                    name: 'RSASSA-PKCS1-v1_5',
+                    hash: 'SHA-256',
+                } as RsaHashedImportParams;
+            case '1.2.840.113549.1.1.5':
+                return {
+                    name: 'RSASSA-PKCS1-v1_5',
+                    hash: 'SHA-1',
+                } as RsaHashedImportParams;
+                case '1.2.840.113549.1.1.12':
+                return {
+                    name: 'RSASSA-PKCS1-v1_5',
+                    hash: 'SHA-384',
+                } as RsaHashedImportParams;
+                case '1.2.840.113549.1.1.13':
+                return {
+                    name: 'RSASSA-PKCS1-v1_5',
+                    hash: 'SHA-512',
+                } as RsaHashedImportParams;
+            case '1.2.840.10045.4.3.2':
+                return {
+                    name: 'ECDSA',
+                    namedCurve: 'P-256',
+                } as EcKeyImportParams;
+            case '1.2.840.10045.4.3.3':
+                return {
+                    name: 'ECDSA',
+                    namedCurve: 'P-384',
+                } as EcKeyImportParams;
+                case '1.2.840.10045.4.3.4':
+                return {
+                    name: 'ECDSA',
+                    namedCurve: 'P-521',
+                } as EcKeyImportParams;
+            case '1.2.840.113549.1.1.2': // MD2
+            case '1.2.840.113549.1.1.3': // MD4
+            case '1.2.840.113549.1.1.4': // MD5
+                alert('Устаревший алгоритм, браузер не хочет его поддерживать: ' + algorithmIdentifier.algorithm.toString());
+                throw new Error('Устаревший алгоритм, браузер не хочет его поддерживать: ' + algorithmIdentifier.algorithm.toString());
+            default:
+                alert('неизвестный алгоритм: ' + algorithmIdentifier.algorithm.toString());
+                throw new Error('неизвестный алгоритм: ' + algorithmIdentifier.algorithm.toString());
+        }
     }
 
     static async validateCert(inputCert: string): Promise<boolean> {
@@ -80,26 +101,32 @@ export class ValidateCertificateComponent implements OnInit {
 
         let tbsCertificate: TBSCertificate = certificate.tbsCertificate;
         let signatureValue: boolean[] = certificate.signatureValue;
-        let subjectPublicKeyInfo = certificate.tbsCertificate.subjectPublicKeyInfo;
+        let subjectPublicKeyInfo: SubjectPublicKeyInfo = certificate.tbsCertificate.subjectPublicKeyInfo;
+
 
         let publicKey = await CryptoModule.gCrypto.subtle.importKey(
             'spki',
             subjectPublicKeyInfo.toBytes(),
-            {
-                name: 'RSASSA-PKCS1-v1_5',
-                //  modulusLength: 2048,
-                //  publicExponent: new Uint8Array([1, 0, 1]),
-                hash: 'SHA-256',
-            },
+            ValidateCertificateComponent.algorithmToCryptoSubtleMapper(certificate.signatureAlgorithm),
             true,
             ['verify']
         );
 
         return CryptoModule.gCrypto.subtle.verify(
-            'RSASSA-PKCS1-v1_5',
+            publicKey.algorithm.name,
             publicKey,
             ValidateCertificateComponent.fromBitString(signatureValue),
             tbsCertificate.toBytes(),
         );
+    }
+
+    async onSubmit() {
+        let inputCert = this.inputCert;
+
+        let result: boolean = await ValidateCertificateComponent.validateCert(inputCert);
+        this.output = result ? 'valid' : 'invalid';
+    }
+
+    ngOnInit(): void {
     }
 }
